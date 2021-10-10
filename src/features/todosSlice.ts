0e5d22bc66d1069cc72146ce80e14563da421917
Melvin Rosales <melvinre1997@gmail.com>
2021-10-09 18:20:24 -0600

    first commit

---
+++ src/features/todosSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface todosState {
+	value: string[];
+}
+
+const initialState: todosState = {
+  value: [],
+};
+
+export const todosSlice = createSlice({
+  name: "reservations",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
+  },
+});
+
+export const { addTodo } = todosSlice.actions;
+
+export default todosSlice.reducer;
